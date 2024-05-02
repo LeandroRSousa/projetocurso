679a54272bfa221df3093173f2992213aa2c47d5
LeandroRSousa <160679250+LeandroRSousa@users.noreply.github.com>
2024-05-02 17:14:23 -0300

    Update scripts.js

--- ProjetoCurso2/scripts.js
+++ ProjetoCurso2/scripts.js
@@ -21,6 +21,34 @@ function toggleMenu() {
 }
 
 
+//SCRIPT PARA MUDAR COR DE FUNDO DE CLARO E ESCURO//
+
+function toggleTheme() {
+    var body = document.body;
+    var elementsToChange = document.querySelectorAll('#intro-msg, h1, h3, h4, #pesquisa-msg, .container-pesquisa');
+    var currentTheme = body.getAttribute('data-theme');
+
+    if (currentTheme === 'dark') {
+        body.setAttribute('data-theme', 'light');
+        body.style.backgroundColor = '#f2f5f8';
+        body.style.color = '#000';
+        for (var i = 0; i < elementsToChange.length; i++) {
+            elementsToChange[i].style.backgroundColor = ''; // Volta para a cor de fundo padrão do CSS
+            elementsToChange[i].style.color = ''; // Volta para a cor do texto padrão do CSS
+        }
+        document.getElementById('tema').innerText = 'Tema Escuro';
+    } else {
+        body.setAttribute('data-theme', 'dark');
+        body.style.backgroundColor = '#222831';
+        body.style.color = 'white';
+        for (var i = 0; i < elementsToChange.length; i++) {
+            elementsToChange[i].style.backgroundColor = '#222831';
+            elementsToChange[i].style.color = 'white';
+        }
+        document.getElementById('tema').innerText = 'Tema Claro';
+    }
+}
+
 
 
 
